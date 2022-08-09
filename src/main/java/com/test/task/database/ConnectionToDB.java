a1335c6aadffbc16fced11baf23b6067794a912c
Elizabeth <liza.ants@gmail.com>
2022-08-09 11:00:43 +0300

    add jsp and entering data from excel to db

--- src/main/java/com/test/task/database/ConnectionToDB.java
+++ src/main/java/com/test/task/database/ConnectionToDB.java
@@ -14,6 +14,8 @@ public class ConnectionToDB {
     private static final String DB_USER = "root";
     private static final String DB_PASSWORD = "root";
 
+    private static final String HIGH_COMMA = "\'";
+
 
     private static Connection connection;
     private static Statement statement;
@@ -50,34 +52,32 @@ public class ConnectionToDB {
             for (String s : insertBankAccount(commonList)){
                 statement.executeUpdate(s);
             }
-
             for (String s : insertGroup(commonList)){
                 statement.executeUpdate(s);
             }
 
 
+
         } catch (SQLException sqlEx) {
+            sqlEx.printStackTrace();
             LoggerProvider.getLOG().error("SQLException occurred");
             return false;
         } finally {
             try {
                 connection.close();
             } catch (SQLException se) {
+                se.printStackTrace();
                 LoggerProvider.getLOG().error("SQLException occurred");
                 return false;
             }
             try {
                 statement.close();
             } catch (SQLException se) {
+                se.printStackTrace();
                 LoggerProvider.getLOG().error("SQLException occurred");
                 return false;
             }
-//            try {
-//                resultSet.close();
-//            } catch (SQLException se) {
-//                LoggerProvider.getLOG().error("SQLException occurred");
-//                return false;
-//            }
+
         }
 
         return true;
@@ -98,13 +98,13 @@ public class ConnectionToDB {
                     "                  sum_circulation_credit,\n" +
                     "                  sum_closing_balance_assets,\n" +
                     "                  sum_closing_balance_liability)" +
-                    "values (" + commonList.get(0).get(i).getId() +
-                    commonList.get(0).get(i).getNameOfClass() +
-                    commonList.get(0).get(i).getOpeningBalanceAssets() +
-                    commonList.get(0).get(i).getOpeningBalanceLiabiblity() +
-                    commonList.get(0).get(i).getCirculationDebit() +
-                    commonList.get(0).get(i).getCirculationCredit() +
-                    commonList.get(0).get(i).getClosingBalanceAssets()
+                    "values (" + commonList.get(0).get(i).getId() +"," + HIGH_COMMA+
+                    commonList.get(0).get(i).getNameOfClass()+ HIGH_COMMA +"," +
+                    commonList.get(0).get(i).getOpeningBalanceAssets()+"," +
+                    commonList.get(0).get(i).getOpeningBalanceLiabiblity()+"," +
+                    commonList.get(0).get(i).getCirculationDebit()+"," +
+                    commonList.get(0).get(i).getCirculationCredit()+"," +
+                    commonList.get(0).get(i).getClosingBalanceAssets()+","
                     +commonList.get(0).get(i).getClosingBalanceLiability() +");";
             allQueries.add(query);
 
@@ -127,14 +127,14 @@ public class ConnectionToDB {
                     "                          closing_balance_assets,\n" +
                     "                          closing_balance_liability,\n" +
                     "                          class_id)" +
-                    "values (" + commonList.get(1).get(i).getId() +
-                    commonList.get(1).get(i).getAccountNumber() +
-                    commonList.get(1).get(i).getOpeningBalanceAssets() +
-                    commonList.get(1).get(i).getOpeningBalanceLiabiblity() +
-                    commonList.get(1).get(i).getCirculationDebit() +
-                    commonList.get(1).get(i).getCirculationCredit() +
-                    commonList.get(1).get(i).getClosingBalanceAssets()
-                    +commonList.get(1).get(i).getClosingBalanceLiability()+
+                    "values (" + commonList.get(1).get(i).getId() +","+
+                    commonList.get(1).get(i).getAccountNumber()+"," +
+                    commonList.get(1).get(i).getOpeningBalanceAssets()+"," +
+                    commonList.get(1).get(i).getOpeningBalanceLiabiblity()+"," +
+                    commonList.get(1).get(i).getCirculationDebit()+"," +
+                    commonList.get(1).get(i).getCirculationCredit() +","+
+                    commonList.get(1).get(i).getClosingBalanceAssets()+","
+                    +commonList.get(1).get(i).getClosingBalanceLiability()+","+
                     commonList.get(0).get(i).getId() + ");";
             allQueries.add(query);
         }
@@ -145,29 +145,34 @@ public class ConnectionToDB {
     public static List<String>  insertGroup(List<List<Entity>> commonList){
         List<String> allQueries = new ArrayList<>();
 
-        for (int i = 0; i < commonList.get(1).size(); i++) {
+        for (int i = 0; i < commonList.get(2).size(); i++) {
 
-            String query = "insert into class (id, group_number,\n" +
-                    "                         sum_opening_balance_assets,\n" +
-                    "                         sum_opening_balance_liability,\n" +
-                    "                         sum_circulation_debit,\n" +
-                    "                         sum_circulation_credit,\n" +
-                    "                         sum_closing_balance_assets,\n" +
-                    "                         sum_closing_balance_liability,\n" +
-                    "                         class_id)" +
-                    "values (" + commonList.get(2).get(i).getId() +
-                    commonList.get(2).get(i).getAccountNumber() +
-                    commonList.get(2).get(i).getOpeningBalanceAssets() +
-                    commonList.get(2).get(i).getOpeningBalanceLiabiblity() +
-                    commonList.get(2).get(i).getCirculationDebit() +
-                    commonList.get(2).get(i).getCirculationCredit() +
-                    commonList.get(2).get(i).getClosingBalanceAssets()
-                    +commonList.get(2).get(i).getClosingBalanceLiability()+
-                    commonList.get(0).get(i).getId() + ");";
-            allQueries.add(query);
+            for(int j= 0; j < 8; j++) {
+                String query = "insert into group_table (id,group_number,\n" +
+                        "                         sum_opening_balance_assets,\n" +
+                        "                         sum_opening_balance_liability,\n" +
+                        "                         sum_circulation_debit,\n" +
+                        "                         sum_circulation_credit,\n" +
+                        "                         sum_closing_balance_assets,\n" +
+                        "                         sum_closing_balance_liability,\n" +
+                        "                         class_id)" +
+                        "values (" + commonList.get(2).get(i).getId() + ","  +
+                        commonList.get(2).get(i).getAccountNumber()  + "," +
+                        commonList.get(2).get(i).getOpeningBalanceAssets() + "," +
+                        commonList.get(2).get(i).getOpeningBalanceLiabiblity() + "," +
+                        commonList.get(2).get(i).getCirculationDebit() + "," +
+                        commonList.get(2).get(i).getCirculationCredit() + "," +
+                        commonList.get(2).get(i).getClosingBalanceAssets() + ","
+                        + commonList.get(2).get(i).getClosingBalanceLiability() + "," +
+                        commonList.get(0).get(j).getId() + ");";
+                allQueries.add(query);
+            }
+
         }
         return allQueries;
     }
+
+
 
 
 
--- src/main/java/com/test/task/excel/ReadFromExcel.java
+++ src/main/java/com/test/task/excel/ReadFromExcel.java
@@ -1,20 +1,14 @@
 package com.test.task.excel;
 
-import com.mysql.cj.util.DataTypeUtil;
 import com.test.task.database.ConnectionToDB;
 import com.test.task.model.Entity;
-import org.apache.logging.log4j.core.tools.picocli.CommandLine;
-import org.apache.poi.hssf.usermodel.HSSFCell;
-import org.apache.poi.hssf.usermodel.HSSFRow;
 import org.apache.poi.hssf.usermodel.HSSFSheet;
 import org.apache.poi.hssf.usermodel.HSSFWorkbook;
 import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.CellType;
-import org.apache.poi.ss.usermodel.CellValue;
 import org.apache.poi.ss.usermodel.Row;
 
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Iterator;
@@ -23,8 +17,8 @@ import java.util.List;
 public class ReadFromExcel {
     private static ReadFromExcel instance;
 
-    public static ReadFromExcel  getInstance(){
-        if (instance == null){
+    public static ReadFromExcel getInstance() {
+        if (instance == null) {
             instance = new ReadFromExcel();
         }
         return instance;
@@ -42,98 +36,92 @@ public class ReadFromExcel {
         List<Entity> listForBankAccount = new ArrayList<>();
         List<Entity> listForClass = new ArrayList<>();
         Iterator<Row> rowIterator = excelSheet.iterator();
+        List<String> classes = new ArrayList<>();
 
-       // for (int i=8; i < excelSheet.getLastRowNum(); i ++){
         while (rowIterator.hasNext()) {
+
             Row row = rowIterator.next();
 
             //For each row, iterate through all the columns
 
 
-            // HSSFRow row = excelSheet.getRow(i);
-            Entity ent = new Entity();
             for (int j = 1; j < row.getLastCellNum(); j = row.getLastCellNum()) {
                 Iterator<Cell> cellIterator = row.cellIterator();
                 //  while (cellIterator.hasNext()) {
                 Cell cell = cellIterator.next();
-
-
-                if (cell.getCellType().equals(CellType.STRING) && cell.getStringCellValue().contains("КЛАСС")
-                 && !cell.getStringCellValue().equals("ПО КЛАССУ")) {
-                    ent.setNameOfClass(cell.toString());
-                    listForClass.add(ent);
-                    break;
-                }else {
-
-
-//                        cellIterator.next();
-//                        break;
-
-//                     if (cell.getCellType().equals(CellType.STRING) && cell.getStringCellValue().isEmpty()){
-//                        cellIterator.next();
-//                        break;
-//                    }
+                Entity ent = new Entity();
+                if (String.valueOf(cell) != "") {
+                    if (cell.getCellType().equals(CellType.STRING) && cell.getStringCellValue().contains("КЛАСС")
+                            && !cell.getStringCellValue().equals("ПО КЛАССУ")) {
+                        classes.clear();
+                        ent.setNameOfClass(cell.getStringCellValue());
+                        classes.add(cell.getStringCellValue());
+                    }
 
 
                     if (row.getCell(row.getFirstCellNum()).toString().equalsIgnoreCase("ПО КЛАССУ")) {
-                        Entity ent1 = new Entity();
-                        ent1.setAccountNumber(null);
-                        ent1.setOpeningBalanceAssets(Double.parseDouble(row.getCell(j).toString()));
-                        ent1.setOpeningBalanceLiabiblity(Double.parseDouble(row.getCell(j + 1).toString()));
-                        ent1.setCirculationDebit(Double.parseDouble(row.getCell(j + 2).toString()));
-                        ent1.setCirculationCredit(Double.parseDouble(row.getCell(j + 3).toString()));
-                        ent1.setClosingBalanceAssets(Double.parseDouble(row.getCell(j + 4).toString()));
-                        ent1.setClosingBalanceLiability(Double.parseDouble(row.getCell(j + 5).toString()));
-                        ent1.setNameOfClass(ent.getNameOfClass());
-                        ent1.setId(id);
+
+                        ent.setAccountNumber(null);
+                        ent.setOpeningBalanceAssets((row.getCell(j).getNumericCellValue()));
+                        ent.setOpeningBalanceLiabiblity((row.getCell(j + 1).getNumericCellValue()));
+                        ent.setCirculationDebit((row.getCell(j + 2).getNumericCellValue()));
+                        ent.setCirculationCredit((row.getCell(j + 3).getNumericCellValue()));
+                        ent.setClosingBalanceAssets((row.getCell(j + 4).getNumericCellValue()));
+                        ent.setClosingBalanceLiability((row.getCell(j + 5).getNumericCellValue()));
+                        ent.setNameOfClass(classes.get(0));
+                        ent.setId(id);
                         id++;
-                        listForClass.add(ent1);
+
+                        listForClass.add(ent);
                         break;
                     }
-                    if (cell.getCellType().equals(CellType._NONE)) {
-                        if (!cell.getCellType().equals(CellType.STRING)) {
 
-                            if (Double.parseDouble(row.getCell(row.getFirstCellNum()).toString()) < 100) {
-                                Entity ent2 = new Entity();
-                                ent2.setAccountNumber(Double.parseDouble(row.getCell(0).toString()));
-                                ent2.setOpeningBalanceAssets(Double.parseDouble(row.getCell(j).toString()));
-                                ent2.setOpeningBalanceLiabiblity(Double.parseDouble(row.getCell(j + 1).toString()));
-                                ent2.setCirculationDebit(Double.parseDouble(row.getCell(j + 2).toString()));
-                                ent2.setCirculationCredit(Double.parseDouble(row.getCell(j + 3).toString()));
-                                ent2.setClosingBalanceAssets(Double.parseDouble(row.getCell(j + 4).toString()));
-                                ent2.setClosingBalanceLiability(Double.parseDouble(row.getCell(j + 5).toString()));
-                                ent2.setNameOfClass(ent.getNameOfClass());
-                                ent2.setId(id);
-                                id++;
+                    if (!cell.getCellType().equals(CellType.STRING)) {
 
+                        if ((row.getCell(row.getFirstCellNum()).getNumericCellValue()) < 100) {
+                            Entity ent2 = new Entity();
+                            ent2.setAccountNumber((row.getCell(0).getNumericCellValue()));
+                            ent2.setOpeningBalanceAssets((row.getCell(j).getNumericCellValue()));
+                            ent2.setOpeningBalanceLiabiblity((row.getCell(j + 1).getNumericCellValue()));
+                            ent2.setCirculationDebit((row.getCell(j + 2).getNumericCellValue()));
+                            ent2.setCirculationCredit((row.getCell(j + 3).getNumericCellValue()));
+                            ent2.setClosingBalanceAssets((row.getCell(j + 4).getNumericCellValue()));
+                            ent2.setClosingBalanceLiability((row.getCell(j + 5).getNumericCellValue()));
+                            ent2.setNameOfClass(classes.get(0));
+                            ent2.setId(id);
+                            id = id + 2;
+
+                            if (ent2.getAccountNumber() != 0.0) {
                                 listForGroupTable.add(ent2);
-                                break;
-
-                            } else if (Double.parseDouble(row.getCell(row.getFirstCellNum()).toString()) > 100) {
-                                Entity ent3 = new Entity();
-                                ent3.setAccountNumber(Double.parseDouble(row.getCell(0).toString()));
-                                ent3.setOpeningBalanceAssets(Double.parseDouble(row.getCell(j).toString()));
-                                ent3.setOpeningBalanceLiabiblity(Double.parseDouble(row.getCell(j + 1).toString()));
-                                ent3.setCirculationDebit(Double.parseDouble(row.getCell(j + 2).toString()));
-                                ent3.setCirculationCredit(Double.parseDouble(row.getCell(j + 3).toString()));
-                                ent3.setClosingBalanceAssets(Double.parseDouble(row.getCell(j + 4).toString()));
-                                ent3.setClosingBalanceLiability(Double.parseDouble(row.getCell(j + 5).toString()));
-                                ent3.setNameOfClass(ent.getNameOfClass());
-                                ent3.setId(id);
-                                id++;
-
-                                listForBankAccount.add(ent3);
-                                break;
-
                             }
-                        } else {
-                            cellIterator.next();
+                            break;
+
+                        } else if (Double.parseDouble(row.getCell(row.getFirstCellNum()).toString()) > 100) {
+                            Entity ent3 = new Entity();
+                            ent3.setAccountNumber((row.getCell(0).getNumericCellValue()));
+                            ent3.setOpeningBalanceAssets((row.getCell(j).getNumericCellValue()));
+                            ent3.setOpeningBalanceLiabiblity((row.getCell(j + 1).getNumericCellValue()));
+                            ent3.setCirculationDebit((row.getCell(j + 2).getNumericCellValue()));
+                            ent3.setCirculationCredit((row.getCell(j + 3).getNumericCellValue()));
+                            ent3.setClosingBalanceAssets((row.getCell(j + 4).getNumericCellValue()));
+                            ent3.setClosingBalanceLiability((row.getCell(j + 5).getNumericCellValue()));
+                            ent3.setNameOfClass(classes.get(0));
+                            ent3.setId(id);
+                            id++;
+
+
+                            listForBankAccount.add(ent3);
+                            break;
+
                         }
+                    } else {
+                        cellIterator.next();
                     }
-                }
+
 
                 }
             }
+        }
 
         commonlist.add(listForClass);
         commonlist.add(listForBankAccount);
@@ -143,7 +131,7 @@ public class ReadFromExcel {
 
     }
 
-    public static void main (String[] args){
+    public static void main(String[] args) {
         try {
             ConnectionToDB.getInstance().getConnection(ReadFromExcel.getInstance().readExcel(" "));
 
